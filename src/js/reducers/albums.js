2198afba8db44dbabfce4a1f61b1052b86f2c7bf
lmikulin <lmikulin@gmail.com>
2016-12-21 11:02:05 -0800

    get album thumbnails

--- src/js/reducers/albums.js
+++ src/js/reducers/albums.js
@@ -6,6 +6,14 @@ export default function reducer(state = albumsInitialState, action) {
       return {...state, fetching: false, albums: action.payload};
     case "FETCH_ALBUMS_RJECTED":
       return {...state, fetching: false, error: action.payload};
+    case "ALBUM_THUMB_FULFILLED":
+      const index = state.albums.findIndex((album) => album.id == action.payload.albumId);
+      if (index >=0) {
+        const album = Object.assign({}, state.albums[index], {thumb: action.payload.thumbnailUrl});
+        const albums = [...state.albums.slice(0, index), album, ...state.albums.slice(index +1)];
+        return {...state, albums: albums};
+      }
+      return state;
   }
   return state;
 }
